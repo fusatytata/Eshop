a783f68f62d6a691daaa1c964a6a9507700e6784
fusatytata <simon.stripes@gmail.com>
2024-04-12 16:55:30 +0200

    Intialize project via Create-React-App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Page from "./components/page";
+
+function App() {
+    return (
+        <div>
+            <Page />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Page() {
+    return (<div>
+        <h1>E-shop</h1>
+        <h2>Cart</h2>
+        <p>Add something to your cart, make it from your heart.</p>
+        <h2>Article (goods) </h2>
+    </div>)
+}
+
+export default Page;
