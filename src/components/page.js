ee1b4b2a7c146ae6871aecedc973bd8fb971cd55
fusatytata <simon.stripes@gmail.com>
2024-04-12 17:18:53 +0200

    Bootstrap wired

--- src/components/page.js
+++ src/components/page.js
@@ -1,12 +1,26 @@
 import React from "react";
 
 function Page() {
-    return (<div>
+    return (
+        <div className="card" HTMLstyle="width: 18rem;">
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                    Go somewhere
+                </a>
+            </div>
+        </div>
+    );
+}
+
+export default Page;
+
+/* /* (<div>
         <h1>E-shop</h1>
         <h2>Cart</h2>
         <p>Add something to your cart, make it from your heart.</p>
         <h2>Article (goods) </h2>
-    </div>)
-}
-
-export default Page;
+    </div>) */
